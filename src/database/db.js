e8fd18d3152bce814d784a5567867625a4caada8
AmanJadon2023 <aman.jadon@geminisolutions.com>
2023-04-27 19:10:37 +0530

    removed my mongodb credentials

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,7 @@ let conn = null;
 
 module.exports = connectDatabase = async ()=> {
     if (conn == null) {
-        conn =await mongoose.connect('mongodb+srv://AmanJadon:Gemini%4012345@cluster0.ud7w084.mongodb.net/todo?retryWrites=true&w=majority')
+        // conn =await mongoose.connect(uri of your database )
         return conn;
     }
     console.log('connection established')
