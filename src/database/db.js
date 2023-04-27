43005f68db80a8a8950662a19b213290766371e1
AmanJadon2023 <aman.jadon@geminisolutions.com>
2023-04-27 19:26:49 +0530

    added .env plugins

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
+// import { uri } from './credentials';
 let conn = null;
 
 module.exports = connectDatabase = async ()=> {
     if (conn == null) {
-        // conn =await mongoose.connect(uri of your database )
+        conn =await mongoose.connect(process.env.DB)
         return conn;
     }
     console.log('connection established')
